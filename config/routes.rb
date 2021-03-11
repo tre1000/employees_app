be26f66e6b786418562090ef3b08a320a4402a37
Tyler Epstein <tylerepstein27@gmail.com>
2021-03-11 13:02:49 -0500

    adds controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/employees" => "employees#index"
+    get "/employees/:id" => "employees#show"
+    post "/employees" => "employees#create"
+    patch "/employees/:id" => "employees#update"
+    delete "/employees/:id" => "employees#destroy"
+  end
 end
